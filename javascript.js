ee0126ded8a41d883431bc145cc7e58fd0dfaa25
James Burt <38007746+Scoce@users.noreply.github.com>
2023-05-10 17:32:49 -0500

    Adds the getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
-console.log("HI");
+/* 
+Gets a random number between 1 and 3 and assigns the text Rock, Paper, or Scissors the choice variable based on the number created.
+*/
+
+function getComputerChoice() {
+    let number = Math.floor((Math.random()* 3)+1);
+    let choice = "";
+    if (number === 1) {
+        choice = "Rock"
+    }
+    else if (number === 2) {
+        choice = "Paper"
+    }
+    else {
+        choice = "Scissors"
+    }
+    return choice;      
+}
+
+
+
+console.log(getComputerChoice());
